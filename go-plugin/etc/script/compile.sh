13f959e929157c782b6ead5f4b241e2f97309054
zonghaishang <yiji@apache.org>
2021-11-14 21:47:42 +0800

    support debug go plugin.

---
+++ go-plugin/etc/script/compile.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+SHELL=/bin/bash
+
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,direct
+
+export PLUGIN_PROJECT=${PROJECT_NAME}
+export SIDECAR_PROJECT=${SIDECAR_PROJECT_NAME}
+
+# update sidecar dependency.
+go mod download
+
+MAJOR_VERSION=$(cat VERSION)
+GIT_VERSION=$(git log -1 --pretty=format:%h)
+
+mkdir -p "/go/src/${PLUGIN_PROJECT}/build/sidecar/binary/"
+rm -rf "/go/src/${PLUGIN_PROJECT}/build/sidecar/binary/*"
+
+go build -mod=readonly -gcflags "all=-N -l" \
+  -ldflags "-B 0x$(head -c20 /dev/urandom | od -An -tx1 | tr -d ' \n') -X main.Version=${MAJOR_VERSION} -X main.GitVersion=${GIT_VERSION}" \
+  -v -o mosn "${SIDECAR_PROJECT}/cmd/mosn/main"
+
+mv mosn "/go/src/${PLUGIN_PROJECT}/build/sidecar/binary/mosn"
