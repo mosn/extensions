c48d5ec77e879888af172f24567e4a21bd42ba28
xingshan <wanghongjia.whj@antgroup.com>
2021-12-14 16:32:15 +0800

    feat: add bolt2sp code

--- go-plugin/plugins/transcoders/bolt2sp/main/bolt2sp.go
+++ go-plugin/plugins/transcoders/bolt2sp/main/bolt2sp.go
@@ -10,7 +10,22 @@ import (
 	"mosn.io/pkg/protocol/http"
 )
 
-var HeaderKeys = []string{"x-mosn-host", "x-mosn-method", "x-mosn-path"}
+const (
+	DefalutClass = "com.alipay.sofa.rpc.core.response.SofaResponse"
+	ClassName    = "class"
+)
+
+var (
+	HeaderKeys    = []string{"x-mosn-host", "x-mosn-method", "x-mosn-path"}
+	Http2BoltCode = map[int]uint16{
+		http.OK:                  bolt.ResponseStatusSuccess,
+		http.BadRequest:          bolt.ResponseStatusError,
+		http.InternalServerError: bolt.ResponseStatusServerException,
+		http.TooManyRequests:     bolt.ResponseStatusServerThreadpoolBusy,
+		http.NotImplemented:      bolt.ResponseStatusNoProcessor,
+		http.RequestTimeout:      bolt.ResponseStatusTimeout,
+	}
+)
 
 type bolt2sp struct {
 	cfg         map[string]interface{}
@@ -39,28 +54,53 @@ func (t *bolt2sp) TranscodingRequest(ctx context.Context, headers api.HeaderMap,
 		return true
 	})
 	// update headers
-	t.updateRequestHeader()
+	targetRequest = t.updateRequestHeader(targetRequest)
 	return http.RequestHeader{RequestHeader: &targetRequest.Header}, buf, trailers, nil
 }
 
 func (t *bolt2sp) TranscodingResponse(ctx context.Context, headers api.HeaderMap, buf api.IoBuffer, trailers api.HeaderMap) (api.HeaderMap, api.IoBuffer, api.HeaderMap, error) {
-	sourceResponse, ok := headers.(http.ResponseHeader)
+	sourceRequest, ok := headers.(http.ResponseHeader)
 	if !ok {
 		return headers, buf, trailers, nil
 	}
-	bufdst := buf.Clone()
-	targetResponse := bolt.NewRpcResponse(t.boltRequest.RequestId, uint16(sourceResponse.StatusCode()), headers, bufdst)
-	return targetResponse, bufdst, trailers, nil
+	targetResponse := bolt.NewRpcResponse(t.boltRequest.RequestId, t.getCode(sourceRequest.StatusCode()), nil, buf)
+	targetResponse.Class = t.getClass()
+	targetResponse.Codec = bolt.JsonSerialize //json
+	return targetResponse, buf, trailers, nil
 }
 
-func (t *bolt2sp) updateRequestHeader() {
+func (t *bolt2sp) updateRequestHeader(req fasthttp.Request) fasthttp.Request {
 	if t.cfg == nil {
-		return
+		return req
 	}
 	for _, key := range HeaderKeys {
-		val := t.cfg[key].(string)
-		if val != "" {
-			t.boltRequest.Header.Set(key, val)
+		val, ok := t.cfg[key].(string)
+		if ok && val != "" {
+			req.Header.Set(key, val)
 		}
 	}
+	return req
+}
+
+func (t *bolt2sp) getClass() string {
+	if t.cfg == nil {
+		return DefalutClass
+	}
+	name, ok := t.cfg[ClassName]
+	if ok {
+		return DefalutClass
+	}
+	n, ok := name.(string)
+	if ok {
+		return DefalutClass
+	}
+	return n
+}
+
+func (t *bolt2sp) getCode(code int) uint16 {
+	boltCode, ok := Http2BoltCode[code]
+	if ok {
+		return boltCode
+	}
+	return bolt.ResponseStatusUnknown
 }
