0c1dc2ea43d30abc6b8570b508e63424cac47a6a
xingshan <wanghongjia.whj@antgroup.com>
2022-04-15 17:18:58 +0800

    feat: update doc

--- go-plugin/configs/transcoders/xr2springcloud/pub_sub.sh
+++ go-plugin/configs/transcoders/xr2springcloud/pub_sub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "pub service http.server@springcloud"
-curl -X POST -d '{"protocolType": "springcloud", "providerMetaInfo": { "appName": "springcloud-provider","properties": {"application": "springcloud-provider","port": "18999" }},"serviceName": "http.server@springcloud"}' localhost:13330/services/publish
+curl -X POST -d '{"protocolType": "springcloud", "providerMetaInfo": { "appName": "springcloud-provider","properties": {"application": "springcloud-provider","port": "18080" }},"serviceName": "http.server@springcloud"}' localhost:13330/services/publish
 
 sleep 2
 
