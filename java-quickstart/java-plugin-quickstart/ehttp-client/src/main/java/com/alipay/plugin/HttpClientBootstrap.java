4e471c289674afe17583cf094df45d89f577bfbe
zonghaishang <yiji@apache.org>
2022-06-22 14:47:08 +0800

    http client

---
+++ java-quickstart/java-plugin-quickstart/ehttp-client/src/main/java/com/alipay/plugin/HttpClientBootstrap.java
@@ -0,0 +1,16 @@
+package com.alipay.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yiji@apache.org
+ */
+@SpringBootApplication
+public class HttpClientBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpClientBootstrap.class, args);
+    }
+
+}
