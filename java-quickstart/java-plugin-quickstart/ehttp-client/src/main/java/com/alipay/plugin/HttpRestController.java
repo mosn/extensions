8a59f98045dc98b45c43871cc4c9f150b2677518
zonghaishang <yiji@apache.org>
2022-06-24 11:18:10 +0800

    add userinfo

--- java-quickstart/java-plugin-quickstart/ehttp-client/src/main/java/com/alipay/plugin/HttpRestController.java
+++ java-quickstart/java-plugin-quickstart/ehttp-client/src/main/java/com/alipay/plugin/HttpRestController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Map;
 
 /**
  * @author yiji@apache.org
@@ -52,6 +54,51 @@ public class HttpRestController {
                         , new HttpEntity<>(headers)
                         , String.class)
                 .getBody();
+    }
+
+    @GetMapping("/userInfo")
+    public String userInfo(@RequestParam(required = false) String port) {
+        HttpHeaders headers = new HttpHeaders();
+
+        // not inject header
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String url = port != null && port.length() > 0
+                ? "http://localhost:" + port + "/userInfo"
+                : "http://localhost:3045/userInfo";
+
+
+        String httpBody = "{\"target\": \"ehttp-provider\"}";
+        HttpEntity<String> entity = new HttpEntity<String>(httpBody, headers);
+
+        return restTemplate.exchange(url
+                        , HttpMethod.POST
+                        , entity
+                        , String.class)
+                .getBody();
+    }
+
+    public class UserModel implements Serializable {
+        private String userId;
+
+        private Map<String, String> parameters;
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+
+        public Map<String, String> getParameters() {
+            return parameters;
+        }
+
+        public void setParameters(Map<String, String> parameters) {
+            this.parameters = parameters;
+        }
     }
 
 }
