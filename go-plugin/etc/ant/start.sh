078b4ccef873d6f3d0104cdcccacb131612c089e
zonghai.szh <zonghai.szh@alibaba-inc.com>
2023-03-02 15:42:41 +0800

    fix linux container start failed

--- go-plugin/etc/ant/start.sh
+++ go-plugin/etc/ant/start.sh
@@ -3,8 +3,8 @@
 BASE_IMAGE=zonghaishang/delve:v1.7.3
 
 DEBUG_PORTS="-p 2345:2345"
-LISTENER_PORTS="-p 12220:12220 -p 12200:12200 -p 30880:30880 -p 30800:30800 -p 10088:10088 -p 10080:10080 -p 34904:34904 -p 15001:15001 -p 15006:15006 -p 3399:3399 -p 13399:13399"
-EXPORT_PORTS="-p 2045:2045 -p 2046:2046 -p 13330:13330 -p 16379:16379 -p 9529:9529 -p 9530:9530 -p 34901:34901"
+LISTENER_PORTS="-p 11001:11001 -p 12220:12220 -p 12200:12200 -p 30880:30880 -p 30800:30800 -p 10088:10088 -p 10080:10080 -p 34904:34904 -p 15001:15001 -p 15006:15006 -p 3399:3399 -p 13399:13399"
+EXPORT_PORTS="-p 34901:34901 -p 13330:13330 -p 2045:2045 -p 2046:2046 -p 16379:16379 -p 9529:9529 -p 9530:9530"
 
 # biz port export
 BIZ_PORTS=" -p 13088:13088 -p 13080:13080"
@@ -27,8 +27,21 @@ DEBUG_MODE=${DLV_DEBUG}
 chmod +x etc/ant/run.sh
 
 # export local ip for mosn
+os_name=$(uname)
+if [[ "$os_name" == "Linux" ]]; then
+    export PUB_BOLT_LOCAL_IP=$(ip -f inet address | grep inet | grep -v docker | grep -v 127.0.0.1 | head -n1 | awk '{print $2}' | awk -F/ '{print $1}')
+  else
+    # default for mac os
+    export PUB_BOLT_LOCAL_IP=$(ipconfig getifaddr en0)
+fi
 export PUB_BOLT_LOCAL_IP=$(ipconfig getifaddr en0)
 echo "host address: ${PUB_BOLT_LOCAL_IP} ->  ${PROJECT_NAME}"
+
+# create mapping logs directory
+if [[ -d ${FULL_PROJECT_NAME}/logs ]]; then
+   rm -rf ${FULL_PROJECT_NAME}/logs
+fi
+mkdir -p ${FULL_PROJECT_NAME}/logs
 
 docker run ${DOCKER_BUILD_OPTS} \
   -u admin --privileged \
