e879cacbc16e757fa0facd8deeba65e5769905c6
zonghai.szh <zonghai.szh@alibaba-inc.com>
2023-03-20 11:33:11 +0800

    template for trace

--- go-plugin/go.mod
+++ go-plugin/go.mod
@@ -1,21 +1,42 @@
 module mosn.io/extensions/go-plugin
 
-go 1.14
+go 1.18
 
 require (
+	github.com/SkyAPM/go2sky v0.5.0
 	github.com/apache/dubbo-go-hessian2 v1.9.2 // dubbo
-	github.com/dubbogo/gost v1.11.25 // indirect
 	github.com/natefinch/lumberjack v2.0.0+incompatible
-	github.com/stretchr/testify v1.7.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.8.1
 	github.com/valyala/fasthttp v1.31.0
-	go.uber.org/atomic v1.9.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	google.golang.org/grpc v1.53.0
 	mosn.io/api v0.0.0-20211217011300-b851d129be01
 	mosn.io/pkg v0.0.0-20211217101631-d914102d1baf
 )
 
+require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dubbogo/gost v1.11.25 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/go-syslog v1.0.0 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
 replace (
 	github.com/klauspost/compress => github.com/klauspost/compress v1.13.5
-
 	github.com/rcrowley/go-metrics => github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475
 )
---
+++ go-plugin/plugins/traces/skywalking/main/tracer.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/SkyAPM/go2sky"
+	la "github.com/SkyAPM/go2sky/reporter/grpc/language-agent"
+	"mosn.io/api"
+	"mosn.io/extensions/go-plugin/pkg/trace"
+	"mosn.io/pkg/log"
+	"mosn.io/pkg/protocol/http"
+)
+
+const (
+	HeadBaggageTraceID       = "Sw8"
+	HeadBaggageLetterTraceID = "sw8"
+	MOSNComponentID          = 5003
+)
+
+var (
+	_ api.Tracer = (*skyTracer)(nil)
+)
+
+type skyTracer struct {
+	cfg            map[string]interface{}
+	tracerProvider *go2sky.Tracer
+}
+
+func TracerBuilder(cfg map[string]interface{}) (api.Tracer, error) {
+	tracerProvider, err := GetTracer(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &skyTracer{
+		cfg:            cfg,
+		tracerProvider: tracerProvider,
+	}, nil
+}
+
+func (tracer *skyTracer) Start(ctx context.Context, request interface{}, startTime time.Time) api.Span {
+	switch req := request.(type) {
+	case http.RequestHeader:
+		return tracer.httpStart(ctx, req, startTime)
+	case api.XFrame:
+		return tracer.frameStart(ctx, req, startTime)
+	}
+	return trace.NewNooSpan()
+}
+
+func (tracer *skyTracer) httpStart(ctx context.Context, header http.RequestHeader, startTime time.Time) api.Span {
+	omsg, ok := tracer.skySpan(ctx, header)
+	if !ok {
+		return nil
+	}
+	requestURI := string(header.RequestURI())
+	entry, nctx, err := tracer.tracerProvider.CreateEntrySpan(ctx, requestURI, func() (string, error) {
+		return omsg, nil
+	})
+	if err != nil {
+		log.DefaultLogger.Errorf("[SkyWalking] [tracer] [http1] create entry span error, err: %v", err)
+		return nil
+	}
+	entry.SetSpanLayer(la.SpanLayer_Http)
+	entry.SetComponent(MOSNComponentID)
+	// new span
+	ospan := NewSpan(ctx, startTime, tracer.tracerProvider)
+	ospan.CreateLocalHttpSpan(nctx, header, entry)
+	return ospan
+}
+
+func (tracer *skyTracer) frameStart(ctx context.Context, xframe api.XFrame, startTime time.Time) api.Span {
+	omsg, ok := tracer.skySpan(ctx, xframe.GetHeader())
+	if !ok {
+		return nil
+	}
+	// ignore heartbeat
+	if xframe.IsHeartbeatFrame() {
+		return nil
+	}
+	entry, nctx, err := tracer.tracerProvider.CreateEntrySpan(ctx, "mosn", func() (string, error) {
+		return omsg, nil
+	})
+	if err != nil {
+		log.DefaultLogger.Errorf("[SkyWalking] [tracer] [http1] create entry span error, err: %v", err)
+		return nil
+	}
+	entry.SetSpanLayer(la.SpanLayer_RPCFramework)
+	entry.SetComponent(MOSNComponentID)
+	// new span
+	ospan := NewSpan(ctx, startTime, tracer.tracerProvider)
+	ospan.CreateLocalRpcSpan(nctx, entry)
+	return ospan
+}
+
+func (tracer *skyTracer) skySpan(ctx context.Context, header api.HeaderMap) (string, bool) {
+	v, ok := header.Get(HeadBaggageTraceID)
+	if ok {
+		return v, ok
+	}
+	v, ok = header.Get(HeadBaggageLetterTraceID)
+	if ok {
+		return v, ok
+	}
+	enabled, _ := tracer.cfg["mosn_generator_span_enabled"].(string)
+	if strings.EqualFold(enabled, "true") {
+		// TODO traceid
+		return v, true
+	}
+	return "", false
+}
